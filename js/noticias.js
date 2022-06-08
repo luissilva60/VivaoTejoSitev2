0132e2f9f707ba71a0d630c6b763f1df761eeab6
Pedro Santos <pedrojgmsantos@gmail.com>
2022-06-08 03:48:09 +0100

    c

--- js/noticias.js
+++ js/noticias.js
@@ -3,7 +3,8 @@ console.log(artigo);
 console.log(artigo.data);
 
 document.getElementById("artigos_title").textContent = artigo.artigos_title
-/*document.getElementById("artigos_subtitle").textContent = artigo.artigos_subtitle*/
+document.getElementById("artigos_subtitle").textContent = artigo.artigos_subtitle
 document.getElementById("artigos_info").textContent = artigo.artigos_info
+document.getElementById("artigos_date").textContent = artigo.artigos_date
 
 document.getElementById("artigos_data").textContent = artigo.data

94de90f174c44d44fb50b689afd05831f25e85ec
Pedro Santos <pedrojgmsantos@gmail.com>
2022-06-08 03:41:25 +0100

    c

--- js/noticias.js
+++ js/noticias.js
@@ -2,5 +2,8 @@ var artigo = JSON.parse(window.localStorage.getItem('artigo'));
 console.log(artigo);
 console.log(artigo.data);
 
-document.getElementById("artigos_name").textContent = artigo.artigos_name
+document.getElementById("artigos_title").textContent = artigo.artigos_title
+/*document.getElementById("artigos_subtitle").textContent = artigo.artigos_subtitle*/
 document.getElementById("artigos_info").textContent = artigo.artigos_info
+
+document.getElementById("artigos_data").textContent = artigo.data
